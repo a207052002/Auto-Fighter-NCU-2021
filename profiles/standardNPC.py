3cf8f89f422a78ae0054ca6aef7f45ac29571012
unknown <a207052002@gmail.com>
2021-04-30 23:13:00 +0800

    change comment, add pvp.py, balance MP

--- profiles/standardNPC.py
+++ profiles/standardNPC.py
@@ -6,16 +6,16 @@ def passive():
     # 有 30 點配點，因此我們只會拿前 30 個字去用
     # 規則之外的字母會被忽略
     # 大小寫一樣
-    # 你的角色起始有 3000 hp、300 dfs、600 atk、300 mp
-    # "H" 代表分配 1 點給 HP，每點 + 100 HP
-    # "D" 代表分配 1 點給 dfs，每點 + 10 dfs(防禦減傷率算法為 (dfs/660))
-    # "A" 代表分配 1 點給 atk，每點 + 80 atk(傷害算法為 atk*(0.5+技能倍率)*(1-dfs/660))
-    # "M" 代表分配 1 點給 mp，每點 + 100 mp(mp 越多可以開越強的技能)
+    # 你的角色起始有 5000 hp、330 dfs、1000 atk、200 mp
+    # "H" 代表分配 1 點給 HP，每點 + 500 HP
+    # "D" 代表分配 1 點給 dfs，每點 + 7 dfs(防禦減傷率算法為 (dfs/660))
+    # "A" 代表分配 1 點給 atk，每點 + 30 atk(傷害算法為 atk*(0.5+技能倍率)*(1-dfs/660))
+    # "M" 代表分配 1 點給 mp，每點 + 10 mp(mp 越多可以用更強的動作)
     # 以下為簡單的範例
     # 字母順序不影響效果呈現，我們只會數對應的字母數量有多少
     # 未滿 30 或是前 30 個內有規格外字母，就代表你放棄了這些天賦點
 
-    passive_str = "H" * 1 + "D" * 1 + "A" * 1 + "M" * 1
+    passive_str = "H" * 0 + "D" * 0 + "A" * 20 + "M" * 10
     return passive_str
 
 def combatLogic(enemy, me, eventmap):
@@ -23,14 +23,15 @@ def combatLogic(enemy, me, eventmap):
     # 我們會幫你把你必須知道的資訊都先放進對應的變數，你們可以參考該變數來決定角色該怎麼動
 
     # eventmap 是一個跟場地大小等長的陣列(list)，每陣列裡面非 0 的數字代表場地上存在的特殊道具
+    # 場地長度 = 17，陣列(0-16)
     # 移動到該格會自動撿起道具
     # 以下為陣列中的數字的意義
     # 1 代表存在 P (Power shot) 撿到後可以一次性開啟無視防禦 25% - 55%(隨角色攻擊提高) 生命的無限距離大招
     # 撿到後透過 return 整數：1 來讓角色使用
     # 2 代表存在 A (Avoid) 撿到後可以一次性開啟一個 buff 持續三回合，期間無條件閃避所有攻擊
     # 以上兩個道具撿到後會存在身上直到使用，已經持有一樣的道具可以再撿但是不會額外增加數量
-    # 3 代表存在 H (Heart) 撿到後可以回覆 20% 的生命(對坦克型角色有很好的效果)
-    # 4 代表存在 M (Mp) 撿到後可以回覆 20% 魔力
+    # 3 代表存在 H (Heart) 撿到後可以回覆 15%+500 的生命(對坦克型角色有很好的效果)
+    # 4 代表存在 M (Mp) 撿到後可以回覆 20% 魔力(MP 高的角色收益高)
     # 每樣道具都非常強大，並且每回合結束後有一定機率隨機產生在地圖上，建議可以考慮以道具的撿取跟使用優先
 
     enemy_atb = enemy.getAtb()
@@ -39,11 +40,14 @@ def combatLogic(enemy, me, eventmap):
     # 敵人當前的血量、魔力、攻擊力、防禦力、位置(0-16)
     enemy_hp = enemy_atb.hp
     enemy_mp = enemy_atb.mp
+    enemy_max_hp = enemy_atb.max_hp
+    enemy_max_mp = enemy_atb.max_mp
     enemy_atk = enemy_atb.atk
     enemy_dfs = enemy_atb.dfs
     enemy_pos = enemy.getPos()
+
     enemy_avoid = enemy.avoid
-    # 敵人是否持有強力攻擊(免費一次9倍無限距離免魔力的大招)
+    # 敵人是否持有強力攻擊
     enemy_power_shot = enemy.power_shot
     # 敵人的閃避 buff 的剩餘回合數，只要比 0 大就會必定閃避所有攻擊，每輪到敵人動一次就會少一回合
     enemy_avoiding = enemy.avoid_buff
@@ -51,30 +55,35 @@ def combatLogic(enemy, me, eventmap):
     # 你的角色當前的血量、魔力、攻擊力、防禦力、位置(0-16)
     my_hp = my_atb.hp
     my_mp = my_atb.mp
+    my_max_hp = my_atb.max_hp
+    my_max_mp = my_atb.max_mp
     my_atk = my_atb.atk
     my_dfs = my_atb.dfs
     my_pos = me.getPos()
 
     my_avoid = me.avoid
-    # 你是否持有強力攻擊(免費一次9倍無限距離免魔力的大招)
+    # 你是否持有強力攻擊
     my_power_shot = me.power_shot
     # 你的閃避 buff 的剩餘回合數，只要比 0 大就會必定閃避所有攻擊，每輪到你動一次就會少一回合
     my_avoiding = me.avoid_buff
 
     # 以下可以開始自由修改與決定你的 AI，上面幫你取好的資訊請盡量不要去動
+    # 最後你 return 在這個 function 的東西會決定你角色該回合的動作
+    # 可以接受的回傳有整數或字串
     # 若回傳數字代表特殊動作如下
     # 0 是冥想，使用後會回 30 mp
     # 1 使用撿到的強力攻擊
     # 2 啟用撿到的迴避能力，持續兩回合(自己回合+敵人回合)，無視所有傷害
-    # 不是以上的數字的話，會當成 0 ，冥想
+    # 數字若不是以上的數字的話，會當成 0 ，冥想
+
     # 若回傳字串
-    # 字串前 30 個字的組合將用來打造你專屬的動作
+    # 字串的組合將用來打造你專屬的動作
     # 大小寫一樣
-    # "A": 代表攻擊力，每個 A + 5% 的傷害並且額外消耗 0.5 的 MP
+    # "A": 代表攻擊力，每個 A + 10% 的傷害並且額外消耗 + 10*(2/3) 的 MP
     # "F": 代表移動效果，每一個 F 會讓你朝敵人移動一格，每個 F 額外消耗 10 的 MP
     # "B": 同樣移動效果，每一個 B 會讓你遠離敵人移動一格，每個 B 額外消耗 10 的 MP
     # 如果 "F" 跟 "B" 同時出現，會互相扣減。比如 "FFB" 等同於 "F"，MP 消耗也視同只有 "F"
-    # "R": 代表攻擊範圍，每一個 R 會讓你該次攻擊的範圍 + 1 格，並額外消耗 5 的 MP
+    # "R": 代表攻擊範圍，每一個 R 會讓你該次攻擊的範圍 + 1 格，並額外消耗 10 的 MP
     # 如果沒有 "R" 代表不進行攻擊，則 "A" 也等同於沒有用
     # 移動距離的上限為 6 格
     # 攻擊距離的上限為 6 格
@@ -93,12 +102,28 @@ def combatLogic(enemy, me, eventmap):
     #----------- 以下請開始撰寫你的程式
 
     action = 0
-    
+    if(my_mp <= 80):
+        action = 0
+    else:
+        if(abs(enemy_pos - my_pos) < 4):
+            if(my_pos <= 3 or my_pos >= 13):
+                action = "FFFFFF"
+            else:
+                action = "B" * ( 5 - abs(enemy_pos - my_pos)) + "RRRRR"
+        elif(abs(enemy_pos - my_pos) <= 5 and my_mp <= 100):
+            action = "R" * abs(enemy_pos - my_pos)
+        elif(abs(enemy_pos - my_pos) <= 5):
+            action = "R" * abs(enemy_pos - my_pos) + "AAAA"
+        elif(abs(enemy_pos - my_pos) > 5):
+            action = "F"
+    if(my_avoid):
+        action = 2
+    if(my_power_shot):
+        action = 1
 
-    # 這邊決定你要使用的技能編號，0-3 選一個。如果不在 0-3 內會視為普攻
     return action
 
 def name():
     # 取個煞氣的名字吧，會顯示在角色上方
-    name = "廢物 NPC"
+    name = "欸欸欸名字可以自己取欸"
     return name
---
+++ pvp.py
@@ -0,0 +1,95 @@
+import sys
+import random
+
+from player import *
+#from profiles import kitting
+#from profiles import advanceTankNPC
+from profiles import *
+from render import Render
+from eventmap import *
+
+RENDER = True
+MAX_POS = 16
+
+def round(p1, p2, atkRange, move, dmg):
+    print(p1.getAtb().hp, p1.getAtb().mp, p2.getAtb().hp,
+          p2.getAtb().mp, atkRange, move, dmg)
+
+
+def build():
+    # pos1, pos2 = (0, 16)
+    pos1, pos2 = random.randint(0, 7), random.randint(9, 16)
+    return [player(pos1, 0), player(pos2, 1)]
+
+def stage():
+    eventmap = Eventmap()
+    p1, p2 = build()
+    p1.setPassives(leftplayer.passive())
+    p1.setActionLambda(leftplayer.combatLogic)
+    p1.setName(leftplayer.name())
+
+    p2.setPassives(rightplayer.passive())
+    p2.setActionLambda(rightplayer.combatLogic)
+    p2.setName(rightplayer.name())
+    # p2.cheat()
+    if(RENDER):
+        renderer = Render(eventmap.getEventMap())
+        renderer.startup([p1, p2])
+    
+
+    print("player0:")
+    p1.getAtb().show()
+    print(p1.getPos())
+    print("player1:")
+    p2.getAtb().show()
+    print(p2.getPos())
+
+    finish = False
+    turn = 0 == random.randint(0,1)
+
+    while(not finish):
+        if(turn) :
+            if(random.randint(0,2) == 0):
+                eventmap.generateEvent([p1.getPos(), p2.getPos()])
+                if(RENDER):
+                    renderer.mapEventSet(eventmap.getEventMap())
+
+            (atk_range, move, dmg, mp, avoid, trigger_event, recover) = p1.action(p2, eventmap)
+            if(RENDER):
+                renderer.render([p1, p2], atk_range, move, dmg, mp, avoid, eventmap.getEventMap(), trigger_event, recover)
+
+            finish = p1.getAtb().hp <= 0 or p2.getAtb().hp <= 0
+
+            if(finish):
+                break
+
+        if(random.randint(0, 2) == 0):
+            eventmap.generateEvent([p1.getPos(), p2.getPos()])
+            if(RENDER):
+                renderer.mapEventSet(eventmap.getEventMap())
+
+        (atk_range, move, dmg, mp, avoid, trigger_event, recover) = p2.action(p1, eventmap)
+        if(RENDER):
+            renderer.render([p2, p1], atk_range, move, dmg, mp, avoid, eventmap.getEventMap(), trigger_event, recover)
+
+        finish = p1.getAtb().hp <= 0 or p2.getAtb().hp <= 0
+
+        turn = True
+
+    if(p1.getAtb().hp <= 0):
+        print("p2: " + p2.getName() + " win")
+        if(RENDER):
+            renderer.end(p2)
+        return 0
+    else:
+        print("p1: " + p1.getName() + " win")
+        if(RENDER):
+            renderer.end(p1)
+        return 1
+
+if(__name__ == '__main__'):
+    loop_time = 1
+    while(loop_time > 0):
+        result = stage()
+        loop_time -= 1
+
