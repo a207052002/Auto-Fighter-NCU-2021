4bcc0261eea162feeee4bc4aff093814fe4bd273
unknown <a207052002@gmail.com>
2021-05-01 00:25:30 +0800

    add special heart drops

--- eventmap.py
+++ eventmap.py
@@ -8,6 +8,10 @@ class Eventmap:
         self.max_pos = MAX_POS_EDGE
         self.max_size = MAX_POS_EDGE + 1
         self.mapInfo = self.max_size * [0]
+        self.round = 0
+
+    def addround(self):
+        self.round += 1
 
     def generateEvent(self, playerPos):
         map_idx = list(range(self.max_size))
@@ -19,6 +23,9 @@ class Eventmap:
         if(len(map_idx) != 0):
             event_pos = random.choice(map_idx)
             event = random.randint(1,4)
+            if(event == 3):
+                if(random.randint(0,30) >= self.round):
+                    event = random.choice([1,2,4])
             self.mapInfo[event_pos] = event
     
     def getEventMap(self):
--- magic.py
+++ magic.py
@@ -21,62 +21,74 @@ def build():
     pos1, pos2 = random.randint(0, 7), random.randint(9, 16)
     return [player(pos1, 0), player(pos2, 1)]
 
+def stage():
+    eventmap = Eventmap()
+    p1, p2 = build()
+    p1.setPassives(leftplayer.passive())
+    p1.setActionLambda(leftplayer.combatLogic)
+    p1.setName(leftplayer.name())
+
+    p2.advanced()
+    p2.setPassives(advanceTankNPC.passive())
+    p2.setActionLambda(advanceTankNPC.combatLogic)
+    p2.setName(advanceTankNPC.name())
+    # p2.cheat()
+    if(RENDER):
+        renderer = Render(eventmap.getEventMap())
+        renderer.startup([p1, p2])
+    
+
+    print("player0:")
+    p1.getAtb().show()
+    print(p1.getPos())
+    print("player1:")
+    p2.getAtb().show()
+    print(p2.getPos())
+
+    finish = False
+
+    while(not finish):
+        if(random.randint(0,2) == 0):
+            eventmap.generateEvent([p1.getPos(), p2.getPos()])
+            if(RENDER):
+                renderer.mapEventSet(eventmap.getEventMap())
+
+        (atk_range, move, dmg, mp, avoid, trigger_event, recover) = p1.action(p2, eventmap)
+        if(RENDER):
+            renderer.render([p1, p2], atk_range, move, dmg, mp, avoid, eventmap.getEventMap(), trigger_event, recover)
+
+        finish = p1.getAtb().hp <= 0 or p2.getAtb().hp <= 0
+
+        if(finish):
+            break
+
+        if(random.randint(0, 2) == 0):
+            eventmap.generateEvent([p1.getPos(), p2.getPos()])
+            if(RENDER):
+                renderer.mapEventSet(eventmap.getEventMap())
+
+        (atk_range, move, dmg, mp, avoid, trigger_event, recover) = p2.action(p1, eventmap)
+        if(RENDER):
+            renderer.render([p2, p1], atk_range, move, dmg, mp, avoid, eventmap.getEventMap(), trigger_event, recover)
+
+        finish = p1.getAtb().hp <= 0 or p2.getAtb().hp <= 0
+
+    if(p1.getAtb().hp <= 0):
+        print("p2: " + p2.getName() + " win")
+        if(RENDER):
+            renderer.end(p2)
+        return 0
+    else:
+        print("p1: " + p1.getName() + " win")
+        if(RENDER):
+            renderer.end(p1)
+        return 1
 
 if(__name__ == '__main__'):
-    p1w = 0
-    p2w = 0
-    for i in range(10000):
-        eventmap = Eventmap()
-        p1, p2 = build()
-        p1.setPassives(leftplayer.passive())
-        p1.setActionLambda(leftplayer.combatLogic)
-        p1.setName(leftplayer.name())
-
-        p2.advanced()
-        p2.setPassives(advanceTankNPC.passive())
-        p2.setActionLambda(advanceTankNPC.combatLogic)
-        p2.setName(advanceTankNPC.name())
-        # p2.cheat()
-        if(RENDER):
-            renderer = Render(eventmap.getEventMap())
-            renderer.startup([p1, p2])
-
-        finish = False
-
-        while(not finish):
-            if(random.randint(0,2) == 0):
-                eventmap.generateEvent([p1.getPos(), p2.getPos()])
-                if(RENDER):
-                    renderer.mapEventSet(eventmap.getEventMap())
-
-            (atk_range, move, dmg, mp, avoid, trigger_event) = p1.action(p2, eventmap)
-            if(RENDER):
-                renderer.render([p1, p2], atk_range, move, dmg, mp, avoid, eventmap.getEventMap(), trigger_event)
-
-            finish = p1.getAtb().hp <= 0 or p2.getAtb().hp <= 0
-
-            if(finish):
-                break
-
-            if(random.randint(0, 2) == 0):
-                eventmap.generateEvent([p1.getPos(), p2.getPos()])
-                if(RENDER):
-                    renderer.mapEventSet(eventmap.getEventMap())
-
-            (atk_range, move, dmg, mp, avoid, trigger_event) = p2.action(p1, eventmap)
-            if(RENDER):
-                renderer.render([p2, p1], atk_range, move, dmg, mp, avoid, eventmap.getEventMap(), trigger_event)
-
-            finish = p1.getAtb().hp <= 0 or p2.getAtb().hp <= 0
-
-        if(p1.getAtb().hp <= 0):
-            print("p2: " + p2.getName() + " win")
-            p2w += 1
-            if(RENDER):
-                renderer.end(p2)
-        else:
-            print("p1: " + p1.getName() + " win")
-            p1w += 1
-            if(RENDER):
-                renderer.end(p1)
-    print("p1: %d wins, p2: %d wins" % (p1w, p2w))
+    ps = [0, 0]
+    loop_time = 1000
+    while(loop_time > 0):
+        result = stage()
+        loop_time -= 1
+        ps[result] += 1
+    print(ps)
